f6cd0d0fc5df5f9eb64e8eba4cced7e5d7b24707
Jonathan Zia <zia@gatech.edu>
2018-10-10 11:43:47 -0400

    Initial Commit

---
+++ ForesightDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForesightDemo
+//
+//  Created by Jonathan Zia on 10/10/18.
+//  Copyright © 2018 Enabyl Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
